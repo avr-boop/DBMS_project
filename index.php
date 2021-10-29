d7c7b6f1bd56ce9a67a4ba9d47444031bd53d04e
Varun Raj <varunrajdec@gmail.com>
2021-10-30 01:51:01 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
+    <meta name="viewport" content="width-device-width">
+    <title>
+        Home Page
+    </title>
+    <meta http-equiv="Content-Type" content="text/html" charset="x-iso-8859-1">
+    <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
+
+</head>
+<body>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <div class="container">1-top</div>
+    <div class="container">1-header</div>
+    <div class="container">1-nav</div>
+</body>
+</html>
