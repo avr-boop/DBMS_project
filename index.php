218474b8a4ad4447086e896edfb10805b15f6f78
Varun Raj <varunrajdec@gmail.com>
2021-10-30 02:29:51 +0530

    basic outline

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
     <meta name="viewport" content="width-device-width">
     <title>
-        Home Page
+        Shopping mart
     </title>
     <meta http-equiv="Content-Type" content="text/html" charset="x-iso-8859-1">
     <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
@@ -13,8 +13,14 @@
 <body>
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.js"></script>
-    <div class="container">1-top</div>
-    <div class="container">1-header</div>
-    <div class="container">1-nav</div>
+    <div class="container-fluid">
+        <div class="container-fluid" style="background-color: #23272b; color: lightgoldenrodyellow">
+            <div class="container">
+            <p style="text-align: end; color: #e2e3e5">Contact for queries</p>
+            </div>
+        </div>
+        <div class="container"><h1><font face="Lucida Handwriting">Shopin mart</font></font></h1></div>
+        <div class="container">1-nav</div>
+    </div>
 </body>
 </html>
