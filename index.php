8667e233c516a04fed6779706bb0b4195ae6098f
Varun Raj <varunrajdec@gmail.com>
2021-10-31 14:04:22 +0530

    adjusted layout

--- index.php
+++ index.php
@@ -4,11 +4,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
     <meta name="viewport" content="width-device-width">
     <title>
-        Shopping mart
+        Tech Shopping mart
     </title>
     <meta http-equiv="Content-Type" content="text/html" charset="x-iso-8859-1">
     <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
+    <style>
+        /* Split the screen in half */
+        .split {
+            position: relative;
+            width: 100%;
+            float: left;
+            height: 100%;
+            background-image: linear-gradient(to bottom right, aliceblue, navajowhite);;
+            z-index: 1010101010
+        }
 
+        /* Control the left side */
+        .split2 {
+            position: relative;
+            width: 100%;
+            float: left;
+            height: 100%;
+            background-image: linear-gradient(to bottom right, azure, lavenderblush);
+            z-index: 1010101010
+        }
+
+        /* Control the right side */
+        .right {
+            right: 0;
+
+        }
+    </style>
 </head>
 <body>
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
@@ -19,7 +45,7 @@
             <p style="text-align: end; color: #e2e3e5">Contact for queries</p>
             </div>
         </div>
-        <div class="container"><h1 align="center"><font face="Lucida Handwriting">Shopin mart</font></font></h1></div>
+        <div class="container"><h1 align="center"><font face="Lucida Handwriting">Tech Shopin mart</font></font></h1></div>
         <div class="container">
             <div class="container" style="background: linear-gradient(100deg,bisque,#7abaff); color: white;">
                 <div class="row">
@@ -48,42 +74,54 @@
 
         </div>
         <br/>
-        <div class="container" >
+        <div class="container" style="white-space: nowrap" >
             <div class="col-sm-2" style="background-color: #f5c6cb;color: #23272b;white-space: nowrap">
-                <h1 align="center">Brand</h1>
-                <center>
-                    <p>
-                        <a href="dell.php" style="color: #23272b;">Dell</a><br/>
-                        <a href="dell.php" style="color: #23272b;">Samsung</a><br/>
-                        <a href="dell.php" style="color: #23272b;">Canon</a><br/>
-                        <a href="dell.php" style="color: #23272b;">Hp</a><br/>
-                    </p>
-                </center>
-                <h1 align="center">Gadget</h1>
-                <center>
-                    <p>
-                        <a href="dell.php" style="color: #23272b;">Mobile</a><br/>
-                        <a href="dell.php" style="color: #23272b;">Laptop</a><br/>
-                        <a href="dell.php" style="color: #23272b;">Camera</a><br/>
-                        <a href="dell.php" style="color: #23272b;">TV</a><br/>
-                    </p>
-                </center>
+                <div class="split">
+                    <div class="centered">
+                        <h1 align="center" >Brand</h1>
+                        <center >
+                            <p>
+                                <a href="dell.php" style="color: #23272b;">Dell</a><br/>
+                                <a href="dell.php" style="color: #23272b;">Samsung</a><br/>
+                                <a href="dell.php" style="color: #23272b;">Canon</a><br/>
+                                <a href="dell.php" style="color: #23272b;">Hp</a><br/>
+                            </p>
+                        </center>
+                    </div>
+                </div>
+                <div class="split2">
+                   <div class="centered">
+                        <h1 align="center">Gadget</h1>
+                        <center >
+                            <p>
+                                <a href="dell.php" style="color: #23272b;">Mobile</a><br/>
+                                <a href="dell.php" style="color: #23272b;">Laptop</a><br/>
+                                <a href="dell.php" style="color: #23272b;">Camera</a><br/>
+                                <a href="dell.php" style="color: #23272b;">TV</a><br/>
+                            </p>
+                        </center>
+                    </div>
+                </div>
             </div>
-            <div class="col-sm-10=" style="white-space: nowrap">
+            <div class="col-sm-10=" style="white-space: nowrap;display: inline-block">
 
                 <div class="col-sm-4 " style="display: inline-block">
                     <center>
                         <img src="img/canoncam.jfif" width="150px" height="150px">
+                        <p>k</p> <!-- Buy now Button-->
                     </center>
+
                 </div>
                 <div class="col-sm-4" style="display: inline-block">
                     <center>
                         <img src="img/canoncam2.jfif" width="150px" height="150px">
+                        <p>k</p>
                     </center>
                 </div>
                 <div class="col-sm-4" style="display: inline-block">
                     <center>
                         <img src="img/dell1.jfif" width="150px" height="150px">
+                        <p>k</p>
                     </center>
                 </div>
                 <br/><br/>.
